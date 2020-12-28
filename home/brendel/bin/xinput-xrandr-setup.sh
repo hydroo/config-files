057fea71c14eb5eacc6d5d56744ed2513ce0b35e
Ronny Brendel <ronnybrendel@gmail.com>
2020-12-28 15:41:46 +0100

    new monitor

--- home/brendel/bin/xinput-xrandr-setup.sh
+++ home/brendel/bin/xinput-xrandr-setup.sh
@@ -3,21 +3,26 @@
 o1="$(xrandr | grep [^s]connected -A1)"
 
 internal="$(echo "$o1" | grep "^ *3840" -B1 | head -n1 | sed "s/^ *[0-9]*:[ +*]*//g" | sed "s/ .*$//g")"
-dell="$(    echo "$o1" | grep "^ *2560" -B1 | head -n1 | sed "s/^ *[0-9]*:[ +*]*//g" | sed "s/ .*$//g")"
-hp="$(      echo "$o1" | grep "^ *1920" -B1 | head -n1 | sed "s/^ *[0-9]*:[ +*]*//g" | sed "s/ .*$//g")"
+dell19="$(    echo "$o1" | grep "^ *2560" -B1 | tail -n2 | sed "s/^ *[0-9]*:[ +*]*//g" | sed "s/ .*$//g")"
+dell21="$(    echo "$o1" | grep "^ *2560" -B1 | head -n1 | sed "s/^ *[0-9]*:[ +*]*//g" | sed "s/ .*$//g")"
 
 touchscreen=$(xinput list | grep Wacom.*Finger | sed "s/.*id=//g" | sed "s/ *\[.*\]*//g")
 
-#echo "$o1"
-#echo "$internal"
-#echo "$dell"
-#echo "$hp"
-#echo $touchscreen
+# echo "$o1"
+# echo "xx"
+# echo "$internal"
+# echo "xx"
+# echo "$dell19"
+# echo "xx"
+# echo "$dell21"
+# echo "xx"
+# echo "$touchscreen"
+# echo "xx"
 
 if [ -n "$internal" ]; then
 	# position the internal display on the bottom
 	# the coordinate system starts in the top-left corner
-	xrandr --output $internal --primary --mode 1920x1080 --pos 2560x1080
+	xrandr --output $internal --primary --mode 1920x1080 --pos 2560x1440
 
 	if [ -n "$touchscreen" ]; then
 		xinput --map-to-output $touchscreen $internal
@@ -28,13 +33,13 @@ else
 	>&2 echo "Error: Internal display not found"
 fi
 
-if [ -n "$dell" ]; then
+if [ -n "$dell19" ]; then
 	# position to the left middle of both other monitors
-	xrandr --output $dell --pos 0x360
+	xrandr --output $dell19 --pos 0x240
 fi
 
-if [ -n "$hp" ]; then
-	xrandr --output $hp --above $internal
+if [ -n "$dell21" ]; then
+	xrandr --output $dell21 --above $internal
 fi
 
 #xrandr --dpi 96
